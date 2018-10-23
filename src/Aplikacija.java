3340cade226461f9e56e34d77a5984b4c5f98a8b
CSU <CSU@CSU-TOSH>
2018-10-23 12:41:31 +0200

    Dodavanje vozila u listu unosom sa tastarue.

--- src/Aplikacija.java
+++ src/Aplikacija.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import automobil.Automobil;
 
@@ -18,8 +19,26 @@ public class Aplikacija {
 		//Metodu add koristimo da dodamo novi element u listu
 		listaAutomobila.add(ford);
 		listaAutomobila.add(new Automobil("Fiat", "Stilo", 5, "bela", 90d));
-
+//		Unos i dodavanje vozila sa konzole!
+		System.out.println("Unesite zeljeno vozilo");
+		Scanner input = new Scanner(System.in);
+		System.out.println("Unesite tip vozila");
+		String tip = input.next();
+		System.out.println("Unesite marku:");
+		String marka = input.next();
+		System.out.println("Unesite broj putnika");
+		Integer brPutnika = input.nextInt();
+		System.out.println("Unesite boju:");
+		String boja = input.next();
+		System.out.println("Unesite cenu: ");
+		Double cena  = input.nextDouble();
+		
+		Automobil unetiAutomobil = new Automobil(marka, tip, brPutnika, boja, cena);
+		System.out.println("Uspesno ste dodali automobil " + unetiAutomobil.informacijeOAutomobilu());
+		listaAutomobila.add(unetiAutomobil);
+		
 		//Koristimo for petlju kako bismo prikazali sve automobile iz liste
+		System.out.println("Vozila na lageru: / Lista vozila:");
 		for (Automobil automobil : listaAutomobila) {
 			System.out.println(automobil.informacijeOAutomobilu());
 		}
